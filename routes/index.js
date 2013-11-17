985f8006b452d98f2df7df5c7132aa9008746854
LukaChepuhin <luka.chepuhin@yandex.ru>
2013-11-17 14:16:00 +0300

    +Database

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 
-/*
- * GET home page.
- */
+var checkAuth = require('middleware/checkAuth');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+module.exports = function(app) {
+
+    app.get('/', require('./frontpage').get);
+
+    app.get('/login', require('./login').get);
+    app.post('/login', require('./login').post);
+
+    app.get('/registration', require('./registration').get);
+    app.post('/registration', require('./registration').post);
+
+    app.post('/logout', require('./logout').post);
+
+    app.get('/chat', checkAuth, require('./service').get);
+
 };
